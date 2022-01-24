3dd090a6c172e5cb4050ac498f59d167a326d63a
Tyler <tylermc404@gmail.com>
2022-01-24 00:31:53 -0500

    Add changes to the main program class.
    Adds classes to the main program, and writes the main
    program logic.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Planet_Generator
 {
@@ -6,18 +7,63 @@ namespace Planet_Generator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberOfPlanets;
 
-            double planetSize;
+            List<Planet> planetList = new List<Planet>();
 
-            for (int i = 0; i < 10; i++)
+            Console.Write("Enter the number of planets to be generated: ");
+            numberOfPlanets = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numberOfPlanets; i++)
             {
-                planetSize = PlanetGen(3, 25);
-
-                Console.WriteLine($"Planet size is {planetSize}");
+                Planet p = new Planet(PlanetNameGen(), Math.Round(PlanetGen(5, 10), 2), Math.Round(PlanetGen(0, 5),2), Convert.ToInt32(Math.Round(PlanetGen(1, 4), 0)));
+                planetList.Add(p);
             }
 
-            Console.ReadLine();
+            Planet randomPlanet = planetList[RandomNum(0, numberOfPlanets)];
+            Console.Write($"The random planet chosen was Planet {planetList.IndexOf(randomPlanet) + 1}, {randomPlanet.name}. Here are the stats:\n" +
+                $"NAME: {randomPlanet.name}\n" +
+                $"SIZE: {randomPlanet.size}\n" +
+                $"POPULATION: {randomPlanet.population}\n" +
+                $"TYPE: ");
+            randomPlanet.PrintPlanetType();
+
+            Console.Write("If you would like to see the stats of any other planet, enter the number now.\n" +
+            "Otherwise, enter 'a' to see all planet's stats, or press any other key to end the program: ");
+
+            string nextChoice = Console.ReadLine();
+
+            while (nextChoice == Convert.ToString('a') || (Convert.ToInt32(nextChoice) <= planetList.Count) && Convert.ToInt32(nextChoice) > 0)
+            {
+                if (nextChoice == Convert.ToString('a'))
+                {
+                    Console.WriteLine("Here are the stats of all planets:\n\n");
+
+                    for (int i = 0; i < planetList.Count; i++)
+                    {
+                        Console.WriteLine("------------------\n");
+                        Console.WriteLine($"PLANET {planetList.IndexOf(planetList[i]) + 1}");
+                        planetList[i].PrintPlanetName();
+                        planetList[i].PrintPlanetSize();
+                        planetList[i].PrintPlanetPop();
+                        planetList[i].PrintPlanetType();
+                    }
+                }
+                else if (Convert.ToInt32(nextChoice) <= planetList.Count && Convert.ToInt32(nextChoice) > 0)
+                {
+                    Console.WriteLine($"------------------\n");
+                    Console.WriteLine($"PLANET {planetList.IndexOf(planetList[Convert.ToInt32(nextChoice) - 1]) + 1}");
+                    planetList[Convert.ToInt32(nextChoice) - 1].PrintPlanetName();
+                    planetList[Convert.ToInt32(nextChoice) - 1].PrintPlanetSize();
+                    planetList[Convert.ToInt32(nextChoice) - 1].PrintPlanetPop();
+                    planetList[Convert.ToInt32(nextChoice) - 1].PrintPlanetType();
+                }
+
+                Console.Write("If you would like to see the stats of any other planet, enter the number now." +
+                    "Otherwise, enter 'a' to see all planet's stats, or press any other key to end the program: ");
+
+                nextChoice = Console.ReadLine();
+            }
         }
 
         public static int RandomNum(int min, int max)
@@ -27,6 +73,17 @@ namespace Planet_Generator
             int randomNumber = rd.Next(min, max);
 
             return randomNumber;
+        }
+
+        
+
+        public static string PlanetNameGen()
+        {
+            string[] nameF = { "Gal", "En", "Mee", "Vel" };
+            string[] nameL = { "ala", "sque", "olo", "nix" };
+
+            return nameF[RandomNum(0, 4)] + nameL[RandomNum(0, 4)];
+
         }
 
         public static double PlanetGen(int min, int max)
